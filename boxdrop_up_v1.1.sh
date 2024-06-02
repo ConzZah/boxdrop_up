09831a2f275057c906e106b218489ce43dcc71bf
ConzZah <69615452+ConzZah@users.noreply.github.com>
2024-06-02 23:52:48 +0200

    Updated boxdrop_up to v1.1
    - cleaned up a bit
    - added some comments
    - fixed some prompts 
    
    Cheers,
    ConzZah.

--- boxdrop_up_v1.1.sh
+++ boxdrop_up_v1.1.sh
@@ -1,39 +1,36 @@
 #!/usr/bin/env bash
 clear
   #========================================
-  # Project: BOXDROP_UP_v1
+  # Project: BOXDROP_UP_v1.1
   # Author:  ConzZah / ©️ 2024
-  # Last Modification: 02.06.2024 / 15:12
+  # Last Modification: 02.06.2024 / 23:48
   #========================================
-# boxdrop_v1
-function boxdrop_v1 {
-echo "BOXDROP_UPLOADER_v1"; echo ""; echo ""
-echo "PASTE FILEPATH"
-read fpath
-echo "PASTE FILENAME"
-read fname
+# boxdrop_v1.1
+function boxdrop_v1.1 {
+echo "======================="
+echo " BOXDROP_UPLOADER_v1.1"
+echo "======================="
+echo ""; echo "PASTE PATH TO FILE"; echo ""; read fpath
+echo ""; echo "PASTE FILENAME"; echo ""; read fname; echo ""
 echo ""
-# fetch new access_token and write it to raw_access_token.txt
-echo "[ ~~~ REFRESHING ACCESS_TOKEN.. ~~~ ]"
-curl -s https://api.dropbox.com/oauth2/token -d refresh_token="$refresh_token" -d grant_type=refresh_token -d client_id="$app_key" -d client_secret="$app_secret" > raw_access_token.txt
-# sed
-sed -i 's/"//g' raw_access_token.txt
-sed -i 's/,//g' raw_access_token.txt
-sed -i 's/{//g' raw_access_token.txt
-sed -i 's/}//g' raw_access_token.txt
-sed -i 's/://g' raw_access_token.txt
-sed -i 's/ token_type bearer expires_in 14400//g' raw_access_token.txt
-sed -i 's/access_token //g' raw_access_token.txt
-access_token=$(<raw_access_token.txt) #load access_token
-rm raw_access_token.txt
-# actual upload
-echo "[ ~~~ UPLOADING "$fname" PLEASE WAIT ~~~ ]"; echo ""; echo ""
+# fetch new access_token, run sed & write result to raw_access_token.txt
+echo "[ ~~~ GETTING ACCESS_TOKEN.. ~~~ ]"
+curl -s https://api.dropbox.com/oauth2/token -d refresh_token="$refresh_token" -d grant_type=refresh_token -d client_id="$app_key" -d client_secret="$app_secret" > raw_access_token.txt 
+# ^ ^ ^ gets access_token
+sed -i 's/{"access_token": "//g' raw_access_token.txt && sed -i 's/", "token_type": "bearer", "expires_in": 14400}//g' raw_access_token.txt 
+# ^ ^ ^ runs sed operations on obtained access_token
+access_token=$(<raw_access_token.txt) 
+# ^ ^ ^ loads access_token
+rm raw_access_token.txt 
+# ^ ^ ^ removes current access_token so there are no conflicts 
+echo ""; echo "[ ~~~ UPLOADING "$fname" PLEASE WAIT ~~~ ]"; echo ""
 cd "$fpath"
 curl -X POST https://content.dropboxapi.com/2/files/upload \
     --header "Authorization: Bearer $access_token" \
     --header "Dropbox-API-Arg: {\"path\": \"/"$fname"\", \"mode\": \"add\", \"strict_conflict\": false}" \
     --header "Content-Type: application/octet-stream" \
     --data-binary @"$fname"
+# actual upload ^ ^ ^
 echo ""; echo ""; echo "[ ~~~~ UPLOAD DONE ~~~~ ]"; echo ""; echo "[ ~~~ PRESS ANY KEY TO EXIT ~~~ ]"; read -n 1 -s; exit
 }
 # final_checks
@@ -45,7 +42,7 @@ cd ~/boxdrop_config
 app_key=$(<app_key.txt)
 app_secret=$(<app_secret.txt)
 refresh_token=$(<refresh_token.txt)
-} 
+}
 # initial_setup
 function initial_setup {
 echo ""; echo "1) INPUT DROPBOX APP KEY"; echo ""; read app_key
@@ -69,4 +66,4 @@ rm _raw_refresh_token.txt; rm __raw_refresh_token.txt
 echo ""; echo "SETUP DONE!"; echo "PRESS ANY KEY TO LAUNCH BOXDROP"; read -n 1 -s; clear; final_checks
 }
 ### LAUNCH ###
-final_checks; boxdrop_v1
+final_checks; boxdrop_v1.1
